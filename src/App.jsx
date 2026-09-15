5f19a0652c7539cf51fe73ce526ce3ec89532481
somaia <somaiaahmadi05@gmail.com>
2026-09-15 16:44:48 +0430

    BlogPost React homework

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import BlogPost from "./BlogPost";
+function App(){
+  return (
+    <div> 
+      <BlogPost/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BlogPost.jsx
@@ -0,0 +1,23 @@
+import "./BlogPost.css";
+
+function BlogPost(){
+    return(
+        <div className="Blog-card">
+            <img className="Blog-image" src="/girl react.png"  alt=" girl learning react"/>
+
+            <div className="Blog-content">
+                <h1> Learn React Components</h1>
+
+                <p className="blog-infor"> By Somaia Ahmadi . sep 14/ 2026</p>
+
+                <p className="blog-desript"> Learn basics of react components and jsx</p>
+
+                <button className="btn"> Read more</button>
+
+            </div>
+             
+        </div>
+    );
+}
+
+export default BlogPost;
